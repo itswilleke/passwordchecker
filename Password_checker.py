fa586a81808b516bc19b972f422df465a13c4ad0
Willeke <102810723+willekebrinkhof@users.noreply.github.com>
2022-04-20 13:38:59 +0200

    Update Password_checker.py
    It now generates a random password and checks for any security breaches.

--- Password_checker.py
+++ Password_checker.py
@@ -1,8 +1,57 @@
 import sys
 import hashlib
 import requests
+import string
+import random
+
+#This section generates a random password
+# characters for password
+alphabets = list(string.ascii_letters)
+digits = list(string.digits)
+special_characters = list("!@#$%^&*()")
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
 
 
+def generate_random_password():
+    # To set a length of the password
+    length = int(input("Enter password length: "))
+    alphabets_count = int(input("Enter alphabets count in password: "))
+    digits_count = int(input("Enter digits count in password: "))
+    special_characters_count = int(
+        input("Enter special characters count in password: "))
+
+    characters_count = alphabets_count + digits_count + special_characters_count
+
+    if characters_count > length:
+        print("Characters total count is greater than the password length")
+        return
+
+    password = []
+    # Pick a random letter
+    for i in range(alphabets_count):
+        password.append(random.choice(alphabets))
+    # Pick a random digit
+    for i in range(digits_count):
+        password.append(random.choice(digits))
+    # Pick a random character
+    for i in range(special_characters_count):
+        password.append(random.choice(special_characters))
+
+    if characters_count < length:
+        random.shuffle(characters)
+        for i in range(length - characters_count):
+            password.append(random.choice(characters))
+
+    # Shuffle the password
+    random.shuffle(password)
+
+    print("".join(password))
+
+
+# Call the function
+password = generate_random_password()
+
+#This section checks for safety of the generated random password
 def request_api_data(query_char):
     url = ' https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
@@ -35,8 +84,9 @@ def main(args):
                 f'{password} was found {count} times... you should probably change your password!')
         else:
             print(f'{password} was NOT found. Carry on!')
-    return 'done!'
+    return 'Your password can be safely used'
 
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
+
